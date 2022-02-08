0d28ed1d775dbbb7eedca13847c6f5e2075187ac
masum_fahim <masum_fahim@nidaansystems.com>
2022-02-07 22:45:18 -0800

    bug fix: api file naming issue

--- src/apis.js
+++ src/apis.js
@@ -1,5 +1,5 @@
-export default api = {
-    weatherURL = (apiKey, city) => {
-        return `api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}`
+export default {
+    weatherURL: (apiKey, city) => {
+        return `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}`
     }
 }
