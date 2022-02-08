1bc634ff600714a4234652fcd0b68c0b5f814069
masum_fahim <masum_fahim@nidaansystems.com>
2022-02-07 22:45:26 -0800

    default data

---
+++ src/utils/default.js
@@ -0,0 +1,44 @@
+export const data = {
+    coord: {
+      lon: -79.4163,
+      lat: 43.7001,
+    },
+    weather: [
+      {
+        id: 804,
+        main: "Clouds",
+        description: "overcast clouds",
+        icon: "04n",
+      },
+    ],
+    base: "stations",
+    main: {
+      temp: 271.74,
+      feels_like: 271.74,
+      temp_min: 270.4,
+      temp_max: 272.68,
+      pressure: 1015,
+      humidity: 82,
+    },
+    visibility: 10000,
+    wind: {
+      speed: 0.89,
+      deg: 234,
+      gust: 3.13,
+    },
+    clouds: {
+      all: 93,
+    },
+    dt: 1644292906,
+    sys: {
+      type: 2,
+      id: 2010632,
+      country: "CA",
+      sunrise: 1644236840,
+      sunset: 1644273380,
+    },
+    timezone: -18000,
+    id: 6167865,
+    name: "Toronto",
+    cod: 200,
+  };
