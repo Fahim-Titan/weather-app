3fca5ba9308cf3e3deae409e63cb04dfbcbd1ee3
masum_fahim <masum_fahim@nidaansystems.com>
2022-02-07 20:30:34 -0800

    HTML restructured | css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import SearchForm from './components/SearchForm/SearchForm';
+const { REACT_APP_APPID } = process.env;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is Header
       </header>
+      <section className="App-main-section">
+        This is the main section
+        <SearchForm/>
+      </section>
+      <footer className="App-footer">
+        Made with care and love by 
+        <span>
+          <a href="https://www.linkedin.com/in/al-masum-fahim/">Al Masum Fahim</a>
+        </span>
+      </footer>
     </div>
   );
 }
